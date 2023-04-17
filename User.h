7ee915122715980d39b51f967778f2d90d61695b
Khaled <loodee1512@gmail.com>
2023-04-17 23:57:30 +0200

    User Class Setup

---
+++ User.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class User
+{
+private:
+	string fullName;
+	int age;
+	string nationalId;
+	char gender;
+	string governorate;
+	string password;
+	bool vaccinated;
+	int dose;
+
+};
+
